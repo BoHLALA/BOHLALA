bdc6bfbe4df8b529324cfae84cff075f48b3cb09
BO-hlala <bo-hlala@hotmail.com>
2025-02-28 11:45:04 +0500

    Update installer.sh

--- script/installer.sh
+++ script/installer.sh
@@ -1,18 +1,32 @@
 #!/bin/sh
 #########################################################
-version= 2.2
-description= BoHlala V2.2 !!!
+version=2.2
+description="BoHLALA" !!!
+
 #########################################################
+
 #########################################################
+PACKAGE_DIR='BOHLALA/main'
 MY_FILE="BoHLALA_FHD.tar.gz"
-MY_TMP_FILE="/var/volatile/tmp/"$MY_FILE
-MY_FILE1="BO_HLALA_skin_V2.2.tar.gz"
-MY_TMP_FILE1="/var/volatile/tmp/"$MY_FILE1
+MY_FILE1="BoHLALA_FHD.tar.gz"
 MY_FILE2="addons1.tar.gz"
-MY_TMP_FILE2="/var/volatile/tmp/"$MY_FILE2
 MY_FILE3="addons2.tar.gz"
-MY_TMP_FILE3="/var/volatile/tmp/"$MY_FILE3
 #########################################################
+
+MY_MAIN_URL="https://raw.githubusercontent.com/BoHLALA/"
+MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE
+MY_URL1=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE1
+MY_URL2=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE2
+MY_URL3=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE3
+MY_TMP_FILE="/tmp/"$MY_FILE
+MY_TMP_FILE1="/tmp/"$MY_FILE1
+MY_TMP_FILE2="/tmp/"$MY_FILE2
+MY_TMP_FILE3="/tmp/"$MY_FILE3
+
+rm -f $MY_TMP_FILE > /dev/null 2>&1
+rm -f $MY_TMP_FILE1 > /dev/null 2>&1
+rm -f $MY_TMP_FILE2 > /dev/null 2>&1
+rm -f $MY_TMP_FILE3 > /dev/null 2>&1
 
 MY_SEP='============================================================='
 echo $MY_SEP
@@ -25,16 +39,10 @@ echo $MY_SEP
 echo 'Downloading '$MY_FILE3' ...'
 echo $MY_SEP
 echo ''
-
-
-wget -O /var/volatile/tmp/BoHLALA_FHD.tar.gz --no-check-certificate "https://github.com/BoHLALA/BOHLALA/raw/main/BoHLALA_FHD.tar.gz"
-wget -O /var/volatile/tmp/BO_HLALA_skin_V2.2.tar.gz --no-check-certificate "https://github.com/BoHLALA/BOHLALA/raw/main/BO_HLALA_skin_V2.2.tar.gz"
-wget -O /var/volatile/tmp/addons1.tar.gz --no-check-certificate "https://github.com/biko-73/BoHLALA/BOHLALA/raw/main/addons1.tar.gz"
-wget -O /var/volatile/tmp/addons2.tar.gz --no-check-certificate "https://github.com/biko-73/BoHLALA/BOHLALA/raw/main/addons2.tar.gz"
-
-rm -rf "/usr/lib/enigma2/python/Plugins/Extensions/BoHLALA_FHD"
-rm -rf "/usr/share/enigma2/BoHLALA_FHD"
-
+wget -T 2 $MY_URL -P "/tmp/"
+wget -T 2 $MY_URL1 -P "/tmp/"
+wget -T 2 $MY_URL2 -P "/tmp/"
+wget -T 2 $MY_URL3 -P "/tmp/"
 
 if [ -f $MY_TMP_FILE ]; [ -f $MY_TMP_FILE1 ]; [ -f $MY_TMP_FILE2 ]; [ -f $MY_TMP_FILE3 ]; then
 
@@ -43,45 +51,44 @@ if [ -f $MY_TMP_FILE ]; [ -f $MY_TMP_FILE1 ]; [ -f $MY_TMP_FILE2 ]; [ -f $MY_TMP
 	echo 'Extracting ...'
 	echo $MY_SEP
 	echo ''
-	tar -xzvf $MY_TMP_FILE -C /
-	tar -xzvf $MY_TMP_FILE1 -C /
-  tar -xzvf $MY_TMP_FILE2 -C /
-  tar -xzvf $MY_TMP_FILE3 -C /
+	tar -xf $MY_TMP_FILE -C /
+	tar -xf $MY_TMP_FILE1 -C /
+	tar -xf $MY_TMP_FILE2 -C /
+	tar -xf $MY_TMP_FILE3 -C /
 	MY_RESULT=$?
 
 	rm -f $MY_TMP_FILE > /dev/null 2>&1
 	rm -f $MY_TMP_FILE1 > /dev/null 2>&1
-  rm -f $MY_TMP_FILE2 > /dev/null 2>&1
-  rm -f $MY_TMP_FILE3 > /dev/null 2>&1
+	rm -f $MY_TMP_FILE2 > /dev/null 2>&1
+	rm -f $MY_TMP_FILE3 > /dev/null 2>&1
 
 	echo ''
+	echo ''
 	if [ $MY_RESULT -eq 0 ]; then
-        echo "########################################################################"
-        echo "#                BoHLALA v_2.2 INSTALLED SUCCESSFULLY                  #"
-        echo "#                      BY TeamNitro - support on                       #"
-        echo "#              https://www.tunisia-sat.com/forums/forums               #"
-        echo "########################################################################"
-        echo "#        Sucessfully Download Please apply from skin selection         #"
-        echo "########################################################################"
+        echo "#########################################################"
+        echo "#    BoHLALA Skin $version INSTALLED SUCCESSFULLY       #"
+        echo "#             BY BoHLALA - support on                   #"
+        echo "#   https://www.tunisia-sat.com/forums/forums/182/      #"
+        echo "#########################################################"
+        echo "#           your Device will RESTART Now                #"
+        echo "#########################################################"		
 		if which systemctl > /dev/null 2>&1; then
 			sleep 2; systemctl restart enigma2
 		else
-			init 4
-			sleep 4 > /dev/null 2>&1
-			init 3
+			init 4; sleep 4; init 3;
 		fi
 	else
 		echo "   >>>>   INSTALLATION FAILED !   <<<<"
 	fi;
-	 echo '**************************************************'
-	 echo '**                   FINISHED                   **'
-	 echo '**************************************************'
-	 echo ''
-	 exit 0
+	echo ''
+	echo '**************************************************'
+	echo '**                   FINISHED                   **'
+	echo '**************************************************'
+	echo ''
+	exit 0
 else
-	 echo ''
-	 echo "Download failed !"
-	 exit 1
+	echo ''
+	echo "Download failed !"
+	exit 1
 fi
-# ----------------------------------------------------------------------------------------------------------
-
+# ------------------------------------------------------------------------------------------------------------
